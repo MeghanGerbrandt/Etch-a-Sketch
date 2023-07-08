d9a83cc3482ff4ceafc0ca1c3730fbfaf96ccc5a
MeghanGerbrandt <132425497+MeghanGerbrandt@users.noreply.github.com>
2023-07-07 23:05:16 -0600

    Finished, added color randomization button and black/white button to switch between the two options.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,18 @@
 let gridSize = document.querySelector(".slider-value").value;
 const gridContainer = document.querySelector(".grid-container");
 let sliderValue = document.querySelector("#size-value");
-let sliderLabel = document.querySelector(".slider-label"); 
+let sliderLabel = document.querySelector(".slider-label");
+const monoButton = document.querySelector(".mono-button");
+const colorButton = document.querySelector(".random-color");
+
+let colorMode = "mono";
 
 sliderValue.addEventListener("input", updateGridSize);
 
 function updateGridSize() {
     gridSize = sliderValue.value;
     sliderLabel.textContent = `Size: ${gridSize} x ${gridSize}`;
-    resetButton();    
+    resetGrid();    
 }
 
 function createGrid(sliderValue) {
@@ -18,17 +22,37 @@ function createGrid(sliderValue) {
         gridContainer.appendChild(row);
 
         for(let j = 0; j < sliderValue; j++) {
-            const sizeOfBox =  400 / gridSize;
+            const sizeOfBox =  350 / gridSize;
             const box = document.createElement("div");
             box.classList.add("box");
             box.style.width = `${sizeOfBox}px`;
             box.style.height = `${sizeOfBox}px`;
             row.appendChild(box);
             box.addEventListener('mouseover', function colorBlack() {
-                box.style.backgroundColor="#384048"; 
+                if (colorMode === "mono") {
+                    box.style.backgroundColor = "#384048";
+                  } else if (colorMode === "randomColor") {
+                    const randomColor = createRandomColor();
+                    box.style.backgroundColor = randomColor;
+                  } 
             })
         } 
     }
+}
+
+function createRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+function monoColor() {
+    colorMode = "mono";
+}
+
+function randomColor() {
+    colorMode = "randomColor";
 }
 
 function resetButton() {
@@ -42,7 +66,6 @@ function resetGrid() {
   }
 
 createGrid(gridSize);
-
 
 
 //createResetButton();
